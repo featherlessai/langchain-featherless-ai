9213f4640f890c958da3354f283e1868e8f0e0a2
DarinVerheijke <darinverheijke@hotmail.com>
2025-05-15 15:58:10 +0200

    update tests

--- tests/integration_tests/test_chat_models.py
+++ tests/integration_tests/test_chat_models.py
@@ -1,9 +1,13 @@
 """Test ChatFeatherlessAi chat model."""
 
 from typing import Type
-
+import pytest
 from langchain_featherless_ai.chat_models import ChatFeatherlessAi
+from langchain_core.messages import AIMessageChunk, BaseMessageChunk
 from langchain_tests.integration_tests import ChatModelIntegrationTests
+from langchain_core.language_models import BaseChatModel
+from langchain_core.tools import BaseTool
+from typing import Optional, Type
 
 
 class TestChatParrotLinkIntegration(ChatModelIntegrationTests):
@@ -12,10 +16,24 @@ class TestChatParrotLinkIntegration(ChatModelIntegrationTests):
         return ChatFeatherlessAi
 
     @property
+    def has_tool_calling(self) -> bool:
+        return False
+
+    @property
+    def tool_choice_value(self) -> str:
+        return "tool_name"
+
+    @property
+    def has_structured_output(self) -> bool:
+        return False
+
+    @property
     def chat_model_params(self) -> dict:
         # These should be parameters used to initialize your integration for testing
         return {
-            "model": "bird-brain-001",
-            "temperature": 0,
-            "parrot_buffer_length": 50,
+            "model": "featherless-ai/Qwerky-72B"
         }
+
+    @pytest.mark.xfail(reason="Not yet supported.")
+    def test_usage_metadata_streaming(self, model: BaseChatModel) -> None:
+        super().test_usage_metadata_streaming(model)
--- tests/unit_tests/test_chat_models.py
+++ tests/unit_tests/test_chat_models.py
@@ -12,10 +12,25 @@ class TestChatFeatherlessAiUnit(ChatModelUnitTests):
         return ChatFeatherlessAi
 
     @property
+    def init_from_env_params(self) -> tuple[dict, dict, dict]:
+        return (
+            {
+                "FEATHERLESSAI_API_KEY": "featherless_api_key",
+                "FEATHERLESSAI_API_BASE": "featherless_api_base",
+            },
+            {
+            },
+            {
+                "featherless_api_key": "featherless_api_key",
+                "featherless_api_base": "featherless_api_base",
+            },
+        )
+
+    @property
     def chat_model_params(self) -> dict:
         # These should be parameters used to initialize your integration for testing
         return {
-            "model": "bird-brain-001",
-            "temperature": 0,
-            "parrot_buffer_length": 50,
+            "model": "featherless-ai/Qwerky-72B",
+            "featherless_api_key": "test_api_key",
+            "featherless_api_base": "http://localhost:8080/test",
         }
